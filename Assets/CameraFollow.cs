a9aa68a4d90598bb5b2271eab128054899ed4b64
MDiaz <madiaz581@gmail.com>
2019-11-14 14:47:42 -0500

    Initial commit

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform playerCam, character, centerPoint;
+
+    private float mouseX, mouseY;
+    public float mouseSensitivity = 10f;
+    public Vector3 offset;
+    public Vector3 characterRotation;
+    public Quaternion characterQuaternion;
+    public float characterRotationX;
+    public float characterRotationY;
+    public float characterRotationZ;
+
+    public float zoom;
+    public float zoomSpeed = 2;
+    public float zoomMin = -2f;
+    public float zoomMax = -10f;
+
+    public float rotationSpeed = 5f;
+
+    void Start()
+    {
+        zoom = zoomMin;
+    }
+
+    void LateUpdate()
+    {
+
+        characterRotationX = character.rotation.x;
+        characterRotationY = character.rotation.y;
+        characterRotationZ = character.rotation.z;
+
+        characterRotation = new Vector3(characterRotationX, characterRotationY, characterRotationZ);
+
+        characterQuaternion = Quaternion.Euler(characterRotationX, characterRotationY, characterRotationZ);
+
+        zoom += Input.GetAxis("CameraZoom") * zoomSpeed * Time.deltaTime;
+        if(zoom > zoomMin)
+        {
+            zoom = zoomMin;
+        }
+        if (zoom < zoomMax)
+        {
+            zoom = zoomMax;
+        }
+        playerCam.transform.localPosition = new Vector3(0, 0, zoom);
+
+        mouseX += Input.GetAxis("CameraHorizontal") * mouseSensitivity;
+        //mouseY -= Input.GetAxis("Mouse Y");
+
+        
+        playerCam.LookAt(centerPoint);
+
+        centerPoint.localRotation = Quaternion.Euler(10, mouseX, 0) * character.rotation;
+
+
+        centerPoint.position = new Vector3(character.position.x, character.position.y, character.position.z) + offset;
+
+        //Quaternion turnAngle = Quaternion.Euler(0, centerPoint.eulerAngles.y, 0);
+
+        mouseX = Mathf.Clamp(mouseX, -60f - centerPoint.rotation.y, 60 + centerPoint.rotation.y); //This restricts camera moving it clamping it at -60 to 60
+
+
+    }
+}
---
+++ Assets/PlayerManager.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float speed;
+    public float maxSpeed;
+    public float handling = 50.0f;
+    private float initialHandling;
+    public float driftHandling;
+    public float drift;
+
+    public float slowDown;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialHandling = handling;
+
+        driftHandling = handling * drift;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetAxis("Submit") >= 1)
+        {
+            Debug.Log("Pressed");
+            if(speed < maxSpeed)
+            {
+                speed += 30 * Time.deltaTime;
+            }
+        } else
+        {
+            Debug.Log("let go");
+            if (speed > 0)
+            {
+                speed -= 20 * Time.deltaTime;
+            }
+        }
+        if(speed < 0)
+        {
+            speed = 0;
+        }
+
+        if(Input.GetAxis("Drift") >= 1)
+        {
+            handling = driftHandling;
+        }
+        else
+        {
+            handling = initialHandling;
+        }
+
+        if(speed > 0 || Input.GetAxis("Drift") >= 1)
+        {
+            float rotation = Input.GetAxis("Horizontal") * handling;
+
+            rotation *= Time.deltaTime;
+
+            transform.Rotate(0, rotation, 0);
+        }
+
+        float translation = speed;
+
+        translation *= Time.deltaTime;
+
+        transform.Translate(0, 0, translation);
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        
+    }
+}
